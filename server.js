288a1a455ef831a89f3c208eed142e9c09ee84a0
Isaac Cisneros <isaac.cisneros@itjuana.com>
2022-09-16 17:22:57 -0700

    connectionString

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const image = require("./controllers/image");
 const db = require("knex")({
   client: "pg",
   connection: {
-    host: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL,
     ssl: true,
   },
 });
